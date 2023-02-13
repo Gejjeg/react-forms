2cbeee4c97232ceef5d53f64bc4e1babd701c5aa
unknown <Gejjeg>
2023-02-13 07:00:56 +0100

    feat: Forms - 05 added

---
+++ src/UncontrolledLogin .js
@@ -0,0 +1,32 @@
+import React from "react";
+
+export class UncontrolledLogin extends React.Component {
+    formSubmit = (event) => {
+        event.preventDefault();
+
+        const username = event.target.elements.username.value;
+        const password = event.target.elements.password.value;
+        const remember = event.target.elements.remember.checked;
+        
+        console.log({
+            username,
+            password,
+            remember
+        })
+
+    }
+
+    render() { 
+        return (
+            <div>
+                <form onSubmit={this.formSubmit}>
+                    <input name="username" />
+                    <input name="password" type="password"/>
+                    <input name="remember" type="checkbox" />
+                    <button type="summit">Login</button>
+                    <button type="reset">Reset</button>
+                </form>
+            </div>
+        )
+    }
+}
